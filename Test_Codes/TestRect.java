eabc0a6bbf90cff750b5208a987fee9fef9820f2
luk3Sky <ganindudananja@gmail.com>
2018-10-19 16:04:00 +0530

    Test classes more

--- Test_Codes/TestRect.java
+++ Test_Codes/TestRect.java
@@ -1,6 +1,9 @@
 class Rect{
     private int len,wid;
     
+    Rect(int x){
+        this.len = x;
+    }
     public void setData(int len, int wid){
         this.len = len;
         this.wid = wid;
@@ -9,12 +12,23 @@ class Rect{
     public int getArea(){
         return this.len*this.wid;
     }
+
+    public int getLen(){
+        return len;
+    }
+
+    public int getWid(){
+        return wid;
+    }
 }
 
 public class TestRect{
     public static void main(String[] args) {
-        Rect r = new Rect();
+        Rect r = new Rect(12);
         r.setData(12, 60);
-        System.out.print(r.getArea());
+        Rect s = new Rect(10);
+        System.out.println(s.getLen());
+        System.out.println(s.getWid());
+        System.out.println(r.getArea());
     }
 }
