268b043ed405d5c50fc7ce30466c68be352b2842
luk3Sky <ganindudananja@gmail.com>
2018-10-26 15:39:00 +0530

    Initial of the LAB2

---
+++ LAB02/E15142Ball.java
@@ -0,0 +1,24 @@
+class Ball{
+    
+    // Private attributes of the Ball 
+    private double x_pos, y_pos, speed, angle_of_speed;
+    public static double time = 0;
+
+    // Constructor of the Ball Class
+    public Ball(double x_position, double y_position, double speed, double angle_of_speed){
+        this.x_pos = x_position;
+        this.y_pos = y_position;
+        this.speed = speed;
+        this.angle_of_speed = angle_of_speed;
+    }
+
+    
+
+}
+
+public class E15142Ball{
+
+    public static void main(String[] args) {
+        
+    }
+}
