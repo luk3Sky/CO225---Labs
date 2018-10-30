0f15a79fce424da7607fb7b106cb31f766f264cd
luk3Sky <ganindudananja@gmail.com>
2018-10-30 16:26:59 +0530

    Task completed

--- LAB02/Ball.java
+++ LAB02/Ball.java
@@ -1,94 +1,94 @@
-import java.lang.Math;
+// import java.lang.Math;
 
-class Ball{
-    // Private attributes of the Ball 
-    private double x_pos, y_pos, speed, angle_of_speed, initial_time;
+// class Ball{
+//     // Private attributes of the Ball 
+//     private double x_pos, y_pos, speed, angle_of_speed, initial_time;
     
-    // Global time for the system
-    public static double system_time = 0;
+//     // Global time for the system
+//     public static double system_time = 0;
 
-    // Constructor of the Ball Class
-    public Ball(double x, double y, double speed, double angleOfSpeedWithX){
-        this.x_pos = x;
-        this.y_pos = y;
-        this.speed = speed;
-        this.angle_of_speed = angleOfSpeedWithX;
-        this.initial_time = system_time;
-    }
+//     // Constructor of the Ball Class
+//     public Ball(double x, double y, double speed, double angleOfSpeedWithX){
+//         this.x_pos = x;
+//         this.y_pos = y;
+//         this.speed = speed;
+//         this.angle_of_speed = angleOfSpeedWithX;
+//         this.initial_time = system_time;
+//     }
 
-    // Getters for the positions
-    public double get_x(){
-        return this.x_pos;
-    }
-    public double get_y(){
-        return this.y_pos;
-    }
-    // Getter for the speed
-    public double get_speed(){
-        return this.speed;
-    }
-    // Getter for the angle with x
-    public double get_angle(){
-        return this.angle_of_speed;
-    }
-    // Getter for the initial time
-    public double get_initial_time(){
-        return this.initial_time;
-    }
+//     // Getters for the positions
+//     public double get_x(){
+//         return this.x_pos;
+//     }
+//     public double get_y(){
+//         return this.y_pos;
+//     }
+//     // Getter for the speed
+//     public double get_speed(){
+//         return this.speed;
+//     }
+//     // Getter for the angle with x
+//     public double get_angle(){
+//         return this.angle_of_speed;
+//     }
+//     // Getter for the initial time
+//     public double get_initial_time(){
+//         return this.initial_time;
+//     }
 
 
-    // Update system time
-    public static void updateTime(double time){
-        system_time = system_time + time;
-    }
+//     // Update system time
+//     public static void updateTime(double time){
+//         system_time = system_time + time;
+//     }
 
-    // Check collision
-    public boolean willCollide(Ball b1){
-        double this_new_pos_x = this.get_x() + (this.get_speed() * (Math.cos(this.get_angle())) *(system_time-this.get_initial_time() ) );
-        double this_new_pos_y = this.get_y() + (this.get_speed() *(Math.sin(this.get_angle())) * (system_time-this.get_initial_time() ) );
-        double other_new_pos_x = b1.get_x() + (b1.get_speed() * (Math.cos(b1.get_angle())) * (system_time-b1.get_initial_time() ) );
-        double other_new_pos_y = b1.get_y() + (b1.get_speed() * (Math.sin(b1.get_angle())) * (system_time-b1.get_initial_time() ) );
+//     // Check collision
+//     public boolean willCollide(Ball b1){
+//         double this_new_pos_x = this.get_x() + (this.get_speed() * (Math.cos(this.get_angle())) *(system_time-this.get_initial_time() ) );
+//         double this_new_pos_y = this.get_y() + (this.get_speed() *(Math.sin(this.get_angle())) * (system_time-this.get_initial_time() ) );
+//         double other_new_pos_x = b1.get_x() + (b1.get_speed() * (Math.cos(b1.get_angle())) * (system_time-b1.get_initial_time() ) );
+//         double other_new_pos_y = b1.get_y() + (b1.get_speed() * (Math.sin(b1.get_angle())) * (system_time-b1.get_initial_time() ) );
         
-        // System.out.println(system_time);
-        // System.out.println(this.get_initial_time());
-        // System.out.println(b1.get_initial_time());
-        // System.out.println(this_new_pos_x);
-        // System.out.println(this_new_pos_y);
-        // System.out.println(other_new_pos_x);
-        // System.out.println(other_new_pos_y);
-        if( (this_new_pos_x == other_new_pos_x) && (this_new_pos_y == other_new_pos_y) ){
-            // System.out.println("Collide");
-            return true;
-        }
-        return false;
-    }
+//         // System.out.println(system_time);
+//         // System.out.println(this.get_initial_time());
+//         // System.out.println(b1.get_initial_time());
+//         // System.out.println(this_new_pos_x);
+//         // System.out.println(this_new_pos_y);
+//         // System.out.println(other_new_pos_x);
+//         // System.out.println(other_new_pos_y);
+//         if( (this_new_pos_x == other_new_pos_x) && (this_new_pos_y == other_new_pos_y) ){
+//             // System.out.println("Collide");
+//             return true;
+//         }
+//         return false;
+//     }
 
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
 
-        Ball b1 = new Ball(0, 1.0, 10.0, 45.0);
+//         Ball b1 = new Ball(0, 1.0, 10.0, 45.0);
         
-        updateTime(5.0);
+//         updateTime(5.0);
         
-        Ball b2 = new Ball(0, 1.0, 20.0, 45.0);
+//         Ball b2 = new Ball(0, 1.0, 20.0, 45.0);
         
-        updateTime(5.0);
+//         updateTime(5.0);
         
-        if ( b1.willCollide(b2) ){
-            System.out.println("B1 and B2 will collide");
-        }else{
-            System.out.println("B1 and B2 won't collide");
-        }
+//         if ( b1.willCollide(b2) ){
+//             System.out.println("B1 and B2 will collide");
+//         }else{
+//             System.out.println("B1 and B2 won't collide");
+//         }
         
-        Ball b3 = new Ball(-10.0, 5.0, 3.0, 30.0);
+//         Ball b3 = new Ball(-10.0, 5.0, 3.0, 30.0);
         
-        updateTime(20.0);
+//         updateTime(20.0);
         
-        if ( b2.willCollide(b3) ){
-            System.out.println("B2 and B3 will collide");
-        }else{
-            System.out.println("B2 and B3 won't collide");
-        }
-        
-    }
+//         if ( b2.willCollide(b3) ){
+//             System.out.println("B2 and B3 will collide");
+//         }else{
+//             System.out.println("B2 and B3 won't collide");
+//         }
 
-}
+//     }
+
+// }
